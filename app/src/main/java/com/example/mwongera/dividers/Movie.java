ebf5557a9df49ea037b337b556e67b86ce69c6e3
mwongeraE <evansmwongera@gmail.com>
2016-03-22 22:40:33 +0300

    movie class

--- app/src/main/java/com/example/mwongera/dividers/Movie.java
+++ app/src/main/java/com/example/mwongera/dividers/Movie.java
@@ -4,4 +4,24 @@ package com.example.mwongera.dividers;
  * Created by mwongera on 3/22/16.
  */
 public class Movie {
+    this.title = title;
+    this.director = director;
+    this.rating = rating;
+    this.imageResourceId = imageResourceId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public int getCoverResourceId() {
+        return imageResourceId;
+    }
 }
